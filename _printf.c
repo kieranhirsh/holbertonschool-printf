586d197f0de95ce155e1217af225b2bfad025415
‘Krisha <8805@holbertonstudents.com>
2024-03-25 05:43:18 +0000

    Updated file

--- _printf.c
+++ _printf.c
@@ -1,10 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - TBA
+ * _printf - prints the string to the command line
+ * @format: character string
  *
  * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
+	int i = 0;
+	int count = 0;
+
+	while (format[i] != NULL)
+	{
+		if (format[i] == '%')
+		{
+		}
+		else
+		{
+			putchar(format[i]);
+			count += 1;
+			i += 1;
+		}
+	}
+	return (count);
 }
