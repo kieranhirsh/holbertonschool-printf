ff373d5ce6c475da1bb88c36eb5caf1d10065458
kieranhirsh <kieranhirsh@gmail.com>
2024-03-25 05:16:10 +0000

    building the very basic framework

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * main - TBA
+ *
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef PRINTF
+#define PRINTF
+
+int _printf(const char *format, ...)
+
+#endif
