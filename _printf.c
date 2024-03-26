d63b50b5baef3183b3ecb0d057239493856206ff
kieranhirsh <kieranhirsh@gmail.com>
2024-03-26 04:33:57 +0000

    now uses function pointers

--- _printf.c
+++ _printf.c
@@ -10,53 +10,44 @@
  */
 int _printf(const char *format, ...)
 {
-	/* op ops[] = {
+	op ops[] = {
 		{'c', print_char},
+		{'d', print_int},
+		{'i', print_int},
 		{'s', print_string},
-		{'%', print_percent}
-	}; */
+		{'%', print_percent},
+		{'\0', NULL}
+	};
 	va_list args;
-	int i = 0, count = 0, is_args = 0;
+	int i = 0, j;
+	int count = 0, is_args = 0;
 
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
 			i += 1;
-			if (format[i] == 'c')
-			{
-				if (is_args == 0)
-					va_start(args, format);
-				is_args = 1;
-				count += print_char(&args);
-			}
-			else if ((format[i] == 'i') || (format[i] == 'd'))
-			{
-				if (is_args == 0)
-					va_start(args, format);
-				is_args = 1;
-				count += print_int(&args);
-			}
-			else if (format[i] == 's')
-			{
-				if (is_args == 0)
-					va_start(args, format);
-				is_args = 1;
-				count += print_string(&args);
-			}
-			else if (format[i] == '%')
-			{
-				if (is_args == 0)
-					va_start(args, format);
-				is_args = 1;
-				count += print_percent(&args);
-			}
-			else
+			j = 0;
+
+			while ((ops[j].type != format[i]) && (ops[j].type != '\0'))
+				j += 1;
+
+			if (ops[j].func == NULL)
 			{
 				putchar('%');
 				putchar(format[i]);
 				count += 2;
 			}
+			else
+			{
+				if (is_args == 0)
+				{
+					va_start(args, format);
+					is_args = 1;
+				}
+				count += ops[j].func(&args);
+			}
+
 			i += 1;
 		}
 		else
